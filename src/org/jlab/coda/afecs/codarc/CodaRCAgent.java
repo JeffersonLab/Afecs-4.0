5c17a311cdd403ed59ae76e72f67110328b8fec4
Vardan Gyurjyan <gurjyan@jlab.org>
2018-10-30 09:45:07 -0400

    debug to check destinationNames

--- src/org/jlab/coda/afecs/codarc/CodaRCAgent.java
+++ src/org/jlab/coda/afecs/codarc/CodaRCAgent.java
@@ -905,11 +905,11 @@ public class CodaRCAgent extends AParent {
 
                         if (msg.getPayloadItem(AConstants.DESTINATIONNAMES) != null) {
                             me.setDestinationNames(msg.getPayloadItem(AConstants.DESTINATIONNAMES).getStringArray());
-                            System.out.println("DDD --------------------");
+                            System.out.println("DDD -------------------------------------------------------");
                             for(String s:me.getDestinationNames()){
                                 System.out.println(s);
                             }
-                            System.out.println("DDD --------------------");
+                            System.out.println("DDD --------------------------------------------------------");
                         }
 
                         if (msg.getPayloadItem("minEventSize") != null)
