f01a6c66d2d70139c25c76f23154e6c29c1c7412
gurjyan <gurjyan@jlab.org>
2020-12-09 13:09:16 -0500

    debugging

--- src/org/jlab/coda/afecs/platform/APlatform.java
+++ src/org/jlab/coda/afecs/platform/APlatform.java
@@ -112,7 +112,7 @@ public class APlatform extends ABase {
         String jdkVersion = System.getProperty("java.version");
         int i = jdkVersion.indexOf("_");
         System.out.println("DDD = "+jdkVersion +" "+i);
-        System.out.println("DDD = "+jdkVersion +" "+jdkVersion.substring(i, 3));
+        System.out.println("DDD = "+jdkVersion +" "+jdkVersion.substring(i, jdkVersion.length()));
         System.out.println("DDD = "+jdkVersion +" "+Integer.parseInt(jdkVersion.substring(i, 3)));
         if (i > 0) {
             try {
