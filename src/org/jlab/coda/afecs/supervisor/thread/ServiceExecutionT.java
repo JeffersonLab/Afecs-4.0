b20abfeeadea7a36fdfb42091d609398d11bbbba
gurjyan <gurjyan@jlab.org>
2025-03-26 16:57:32 -0400

    Debugging Hall-A problem...

--- src/org/jlab/coda/afecs/supervisor/thread/ServiceExecutionT.java
+++ src/org/jlab/coda/afecs/supervisor/thread/ServiceExecutionT.java
@@ -636,6 +636,7 @@ public class ServiceExecutionT implements Runnable {
                     } else {
                         pr = pu;
                     }
+                    System.out.println("FFF: "+ su+" "+ pr);
                     if (su.contains(pr) ||
                             (serviceName.equals("CodaRcStartRun") && bp.getBefore().equals(AConstants.prestarted))) {
 
