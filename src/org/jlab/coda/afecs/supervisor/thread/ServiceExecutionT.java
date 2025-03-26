4ca396df6fe00a1ddd11df0d5ed52f5773cb4e9c
gurjyan <gurjyan@jlab.org>
2025-03-26 13:51:47 -0400

    Debugging Hall-A problem...

--- src/org/jlab/coda/afecs/supervisor/thread/ServiceExecutionT.java
+++ src/org/jlab/coda/afecs/supervisor/thread/ServiceExecutionT.java
@@ -650,10 +650,13 @@ public class ServiceExecutionT implements Runnable {
                                     " Starting process = " + bp.getName());
                         }
                         // execute scripts before the state transition
+                        if (bp.getSendPackages().isEmpty()) {
+                            System.out.println("FFF =======================> " );
 
-//                        owner.pm.executeProcess(bp, owner.myPlugin, owner.me); //12.18.24 VG
-                        owner.pm.executeSup2RCProcess(bp, owner.myPlugin, owner);
-
+                            owner.pm.executeProcess(bp, owner.myPlugin, owner.me);
+                        } else {//12.18.24 VG
+                            owner.pm.executeSup2RCProcess(bp, owner.myPlugin, owner);
+                        }
 //                        owner.reportAlarmMsg(owner.me.getSession() + "/" + owner.me.getRunType(),
 //                                owner.myName,
 //                                1,
